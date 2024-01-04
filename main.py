4722bc23e31cd7b8988461069d52ff9a9bac79eb
Seaver Olson <46256937+seaver-olson@users.noreply.github.com>
2024-01-03 21:43:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 import pandas as pd
 
-df = pd.read_csv('mushrooms.csv')
 
 def user_input():
     cap_color = input('What is the cap color? brown=n,buff=b,cinnamon=c,gray=g,green=r,pink=p,purple=u,red=e,white=w,yellow=y')
     cap_shape = input('What is the cap shape? bell=b,conical=c,convex=x,flat=f, knobbed=k,sunken=s')
-    odor = input('What is the odor? ')
-    gill_color = input('What is the gill color? ')
-    gill_size = input('What is the gill size? ')
-    stalk_shape = input('What is the stalk shape? ')
+    odor = input('What is the odor? almond=a,anise=l,creosote=c,fishy=y,foul=f,musty=m,none=n,pungent=p,spicy=s')
+    gill_color = input('What is the gill color? black=k,brown=n,buff=b,chocolate=h,gray=g, green=r,orange=o,pink=p,purple=u,red=e,white=w,yellow=y')
+    gill_size = input('What is the gill size? broad=b,narrow=n')
+    stalk_shape = input('What is the stalk shape? enlarging=e,tapering=t')
     description = {'cap-color': cap_color, 'cap-shape': cap_shape, 'odor': odor, 'gill-color': gill_color, 'gill-size': gill_size, 'stalk-shape': stalk_shape}
     return description
+#q:UnboundLocalError: cannot access local variable 'df' where it is not associated with a value
+#Q: how to fix the error abovE?
+#A: make df a global variable
 
-def shrink(description, df):
+def shrink(description):
     '''
     shrinks down possible rows
     '''
-    df = df[df['cap-color'] == description['cap-color']]
-    df = df[df['cap-shape'] == description['cap-shape']]
-    df = df[df['odor'] == description['odor']]
-    df = df[df['gill-color'] == description['gill-color']]
-    df = df[df['gill-size'] == description['gill-size']]
-    df = df[df['stalk-shape'] == description['stalk-shape']]
+    df = pd.read_csv('mushrooms.csv')
+    for key in description:
+        df = df[df[key] == description[key]]
     return df
-    
+
 def isPoisonous(df):
     '''
     returns the percent chance of being poisonous
     '''
     poisonous = df[df['class'] == 'p']
     edible = df[df['class'] == 'e']
-    return len(poisonous) / (len(poisonous) + len(edible))
+    try:
+        return "percent chance that the mushroom is poisonous: " + str((len(poisonous) / (len(poisonous) + len(edible)))*100)
+    except ZeroDivisionError:
+        return "No mushrooms found with that description"
 
 def main():
     description = user_input()
-    df = shrink(description, df)
+    df = shrink(description)
     print(isPoisonous(df))
     
+
+main()
